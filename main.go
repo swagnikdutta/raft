a5aa5bee76cb4ed697e1fb03e006ea51ce115408
Swagnik Dutta <swagnikd@gmail.com>
2021-10-01 20:17:58 +0530

    starting fresh

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-
-	"github.com/swagnikdutta/raft/raft"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go raft.CreateNewServer(&wg)
-	go raft.CreateNewServer(&wg)
-	wg.Wait()
 
-	fmt.Println("test")
 }
---
+++ raft/cluster.go
@@ -0,0 +1,4 @@
+package raft
+
+type Cluster struct {
+}
