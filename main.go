50b72f5ea55f04a5fc758485f12646db4e686f2d
Swagnik Dutta <swagnikd@gmail.com>
2021-10-02 14:29:23 +0530

    added seed to get better randomised results

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/swagnikdutta/raft/raft"
 )
 
 func main() {
 	// create cluster with 3 servers
+	rand.Seed(time.Now().UnixNano())
 	raft.CreateCluster(3)
 }
