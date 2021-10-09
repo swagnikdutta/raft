423af0f0c6dfd2f17021a136511b092ecd269871
Swagnik Dutta <swagnikd@gmail.com>
2021-10-09 13:37:32 +0530

    bug fix, rpc server initialization was missing before serving connections

--- raft/server.go
+++ raft/server.go
@@ -18,10 +18,9 @@ type Server struct {
 	timer   *time.Timer
 	state   string
 
-	listener  net.Listener
-	rpcServer *rpc.Server
-
-	peerClients map[string]*rpc.Client
+	listener    net.Listener           // to listen for incoming connections
+	rpcServer   *rpc.Server            // to server incoming connections
+	peerClients map[string]*rpc.Client // a client object for each peer it wants to communicate with
 
 	wg sync.WaitGroup
 }
@@ -31,9 +30,11 @@ type Server struct {
 func (s *Server) ConnectToPeers(peers []*Server) {
 	for i := 0; i < len(peers); i++ {
 		peer := peers[i]
+
 		if s.peerClients[peer.id] == nil {
 			listenerAddress := peer.listener.Addr()
 			client, err := rpc.Dial(listenerAddress.Network(), listenerAddress.String())
+
 			if err != nil {
 				fmt.Println("Error connecting to peer", err)
 			}
@@ -45,6 +46,8 @@ func (s *Server) ConnectToPeers(peers []*Server) {
 // Functions
 
 func NewServer(serverCount int, wg *sync.WaitGroup) *Server {
+	var err error
+
 	server := new(Server)
 	server.id = uuid.New().String()
 	server.state = FOLLOWER
@@ -53,8 +56,10 @@ func NewServer(serverCount int, wg *sync.WaitGroup) *Server {
 		currentTerm: 1,
 	}
 
+	server.rpcServer = rpc.NewServer()
+	// server.rpcServer.Register(server.CM)
+
 	// Attaching listener
-	var err error
 	server.listener, err = net.Listen("tcp", ":0")
 	if err != nil {
 		fmt.Println("Error while attaching listener on the server")
@@ -67,17 +72,19 @@ func NewServer(serverCount int, wg *sync.WaitGroup) *Server {
 		defer server.wg.Done()
 
 		for {
-			conn, err := server.listener.Accept() // blocks
-			fmt.Println("Got a connection", conn)
+			conn, err := server.listener.Accept() // TODO: close the connection at some point
+
 			if err != nil {
 				fmt.Println("Error listening for incoming connections")
 				log.Fatal(err)
 			}
-			// server.wg.Add(1)
-			// go func() {
-			// 	defer server.wg.Done()
-			// 	server.rpcServer.ServeConn(conn)
-			// }()
+
+			// handle the connection in a new goroutine and go back to accepting new incoming connections.
+			server.wg.Add(1)
+			go func(c net.Conn) {
+				defer server.wg.Done()
+				server.rpcServer.ServeConn(c) // TODO: shut this down at some point
+			}(conn)
 		}
 	}()
 
